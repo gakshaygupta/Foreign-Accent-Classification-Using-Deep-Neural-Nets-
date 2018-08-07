ed50c630d6c1c6590f9c7f05ccab84e9ea23f9f2
Akshay gupta <gakshaygupta@hotmail.com>
2018-08-07 08:42:27 +0530

    good accuracy at start

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ import numpy as np
 #import generator
 from imblearn.over_sampling import SMOTE, ADASYN
 #importing the training and testing data 
-k=open(r"F:\datasets\cv_corpus_v1\dataset1.plk","rb")
+k=open(r"F:\datasets\cv_corpus_v1\dataset2500.plk","rb")
 dataset=pickle.load(k)
 X=[]
 Y=[]
@@ -30,22 +30,26 @@ Conv2d=keras.layers.Conv2D
 GRU=keras.layers.GRU
 Dropout=keras.layers.Dropout
 pooling=keras.layers.AveragePooling2D
-model.add(Conv2d(32,[4,4],input_shape=[96,128,3],activation="relu"))
+l2=keras.regularizers.l2
+
+model.add(Conv2d(64,[2,2],input_shape=[96,128,3],activation="relu",kernel_regularizer=l2(0.01)))
+
 model.add(Dropout(0.1))
 model.add(pooling(pool_size=(2,2)))
-model.add(Conv2d(64,[5,5],activation="relu"))
+model.add(Conv2d(64,[5,5],activation="relu",kernel_regularizer=l2(0.01)))
 model.add(Dropout(0.1))
 model.add(pooling(pool_size=(3,3)))
-model.add(Conv2d(32,[3,3],activation="relu"))
+model.add(Conv2d(32,[3,3],activation="relu",kernel_regularizer=l2(0.01)))
 model.add(Dropout(0.1))
 model.add(pooling(pool_size=(2,2)))
 model.add(Conv2d(32,[6,1],activation="relu"))
 model.add(Dropout(0.1))
 model.add(keras.layers.Reshape([8,32]))
 model.add(GRU(100))
-#model.add(keras.layers.Reshape([100,1]))
-#model.add(GRU(50))
+#model.add(keras.layers.Reshape([150,1]))
+#model.add(GRU())
 model.add(keras.layers.Dense(100,activation="relu"))
+model.add(Dropout(0.3))
 model.add(keras.layers.Dense(3,activation="softmax"))
 model.compile("Adagrad",loss='categorical_crossentropy',metrics=["accuracy"])
-model.fit(np.array(X_train),np.array(Y_train),1,500,validation_data=[np.array(X_test),np.array(Y_test)],shuffle=True)
+model.fit(np.array(X_train),np.array(Y_train),50*2,400,validation_data=[np.array(X_test),np.array(Y_test)],shuffle=True)
